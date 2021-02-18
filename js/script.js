c71f0c4c239b22d78b25073c518f9c0d5bcc2e5d
Davide <bibbodavide@gmail.com>
2021-02-18 14:46:37 +0100

    Ho Aggiunto il logo e le icone e le ho rese dinamiche nel js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var header = new Vue({
+  el: "header",
+
+  data: {
+
+    logoImg: {
+      name: "logo",
+      path: "avadabarbers-logo-x1.png"
+    },
+    
+    navIcons: [
+      {
+        prefix: "fas ",
+        type: "fa-shopping-cart",
+        active: false
+      },
+      {
+        prefix: "fas ",
+        type: "fa-bars",
+        active: false
+      }
+    ]
+  },
+
+  methods: {
+
+
+  }
+});
+Vue.config.devtools = true;
